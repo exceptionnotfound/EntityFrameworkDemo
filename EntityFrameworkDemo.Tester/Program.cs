fcddfb9aadec3d3bb3334bc96f874a845dc04c7d
exceptionnotfound <mpjones428@gmail.com>
2015-05-14 14:08:11 -0700

    Added CodeFirst from Database example

---
+++ EntityFrameworkDemo.Tester/Program.cs
@@ -0,0 +1,30 @@
+﻿using EntityFrameworkDemo.ModelFirst.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDemo.Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char input = 'b';
+            while (input != 'q')
+            {
+                Console.Write("Select a test:");
+                var testSelection = Console.ReadLine().ToLower().First();
+                switch (testSelection)
+                {
+                    case 'l':
+                        CustomerManager.LeavingContext(new List<string>() {"BLONP", "CHOPS", "LINOD"});
+                        break;
+                }
+
+                Console.WriteLine(Environment.NewLine);
+            }
+        }
+    }
+}
