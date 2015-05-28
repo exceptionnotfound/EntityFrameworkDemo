baa55844c6123b58ebf8d8f0394d9018c8b565ca
exceptionnotfound <mpjones428@gmail.com>
2015-05-28 10:18:20 -0700

    Removed Survey

--- EntityFrameworkDemo/ModelFirst/Northwind/Survey.cs
+++ EntityFrameworkDemo/ModelFirst/Northwind/Survey.cs
@@ -1,4 +1,4 @@
-﻿using EntityFrameworkDemo.CodeFirst.Northwind;
+﻿using EntityFrameworkDemo.CodeFirst.CodeFirst.Northwind;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EntityFrameworkDemo.CodeFirst.Northwind
+namespace EntityFrameworkDemo.CodeFirst.CodeFirst.Northwind
 {
     public partial class Survey
     {
